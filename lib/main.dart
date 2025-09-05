fd760b8fd77ba13ed2630227ed430ed1755c8764
Daniel Valido <jvalidojeronimo@gmail.com>
2025-09-05 13:59:45 -0600

    structure updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -9,10 +14,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      title: 'AgroMetrics',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('AgroMetrics - App para Tractoristas'),
         ),
       ),
     );
